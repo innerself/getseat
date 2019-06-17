8a951f6631aed87f67e09cadddc950370e9e3f65
Sergey Petrin <spetrin@gmail.com>
2019-06-17 17:56:33 +0300

    Transfer commit

--- getseat/models.py
+++ getseat/models.py
@@ -1,3 +1,58 @@
-from django.db import models
+import datetime
+from dataclasses import dataclass
 
-# Create your models here.
+from django.db import models
+from django.db.models import ForeignKey
+
+
+class Station(models.Model):
+    name = models.CharField(max_length=200)
+    code = models.CharField(max_length=10)
+
+
+class Train(models.Model):
+    departure_station = ForeignKey(
+        Station,
+        on_delete=models.CASCADE,
+        related_name='trains',
+    )
+    arrival_station = ForeignKey(
+        Station,
+        on_delete=models.CASCADE,
+        related_name='trains',
+    )
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+
+class TrainSearch:
+    def __init__(self,
+        departure_station: Station,
+        arrival_station: Station,
+        date: datetime.date):
+
+        self.departure_station = departure_station
+        self.arrival_station = arrival_station
+        self.date = date
+
+    def get_schedule(self):
+        pass
+
+    def _raw_page(self):
+        date_format = '%d.%m.%Y'
+        search_root = 'https://www.tutu.ru/poezda/rasp_d.php'
+        dept_st = f'nnst1={self.departure_station.code}'
+        arr_st = f'nnst2={self.arrival_station.code}'
+        dt = f'date={self.date.strftime(date_format)}'
+        search_url = f'{search_root}?{dept_st}&{arr_st}&{dt}'
+
+
+
+
+    def _get_raw_schedule(self):
+
+
+
+
+    def _parse_schedule(self):
+        pass

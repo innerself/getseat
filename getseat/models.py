940ad31e3b43a863ed5311ba66c766317f7b7069
Sergey Petrin <spetrin@gmail.com>
2019-07-12 22:37:50 +0300

    Transfer commit

--- getseat/models.py
+++ getseat/models.py
@@ -1,25 +1,31 @@
 import datetime
-from dataclasses import dataclass
 
+from bs4 import BeautifulSoup
 from django.db import models
 from django.db.models import ForeignKey
+from selenium.webdriver import Firefox, FirefoxProfile
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.webdriver import WebDriver
 
 
 class Station(models.Model):
     name = models.CharField(max_length=200)
     code = models.CharField(max_length=10)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Train(models.Model):
     departure_station = ForeignKey(
         Station,
         on_delete=models.CASCADE,
-        related_name='trains',
+        related_name='departing_trains',
     )
     arrival_station = ForeignKey(
         Station,
         on_delete=models.CASCADE,
-        related_name='trains',
+        related_name='arriving_trains',
     )
     departure_time = models.DateTimeField()
     arrival_time = models.DateTimeField()
@@ -27,18 +33,19 @@ class Train(models.Model):
 
 class TrainSearch:
     def __init__(self,
-        departure_station: Station,
-        arrival_station: Station,
-        date: datetime.date):
+                 departure_station: Station,
+                 arrival_station: Station,
+                 date: datetime.date):
 
         self.departure_station = departure_station
         self.arrival_station = arrival_station
         self.date = date
 
     def get_schedule(self):
-        pass
+        raw_schedule = self._get_raw_page()
+        schedule = self._parse_schedule(raw_schedule)
 
-    def _raw_page(self):
+    def _get_raw_page(self) -> str:
         date_format = '%d.%m.%Y'
         search_root = 'https://www.tutu.ru/poezda/rasp_d.php'
         dept_st = f'nnst1={self.departure_station.code}'
@@ -46,13 +53,35 @@ class TrainSearch:
         dt = f'date={self.date.strftime(date_format)}'
         search_url = f'{search_root}?{dept_st}&{arr_st}&{dt}'
 
+        browser = self._open_browser()
+        browser.get(search_url)
+
+        return browser.page_source
+
+    def _parse_schedule(self, raw_schedule):
+        soup = BeautifulSoup(raw_schedule, 'html.parser')
+        schedule_cards = soup.find_all(
+            'div', {'class': 'b-train__schedule__train_card'}
+        )
+
+        print()
+
+    @staticmethod
+    def _open_browser() -> WebDriver:
+
+        opt = Options()
+        # opt.headless = True
+
+        profile = FirefoxProfile()
+        profile.set_preference("permissions.default.image", 2)
+
+        browser = Firefox(
+            executable_path='./geckodriver',
+            options=opt,
+            firefox_profile=profile,
+        )
+
+        return browser
 
 
 
-    def _get_raw_schedule(self):
-
-
-
-
-    def _parse_schedule(self):
-        pass
--- getseat/tests.py
+++ getseat/tests.py
@@ -1,3 +1,54 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+from getseat.models import Station, Train, TrainSearch
+
+
+class TrainSearchTests(TestCase):
+
+    def setUp(self) -> None:
+        Station.objects.create(
+            name='Москва',
+            code='2000000',
+        )
+
+        Station.objects.create(
+            name='Тарусская',
+            code='2001062',
+        )
+
+    def tearDown(self) -> None:
+        
+
+    def test_get_raw_page(self):
+        search = TrainSearch(
+            departure_station=Station(name='Москва', code='2000000'),
+            arrival_station=Station(name='Тарусская', code='2001062'),
+            date=datetime.date(2019, 7, 12),
+        )
+
+        self.assertTrue('j-content s-disabled' in search._get_raw_page())
+
+        search = TrainSearch(
+            departure_station=Station(name='Москва', code='9999999'),
+            arrival_station=Station(name='Тарусская', code='1234567'),
+            date=datetime.date(2019, 7, 12),
+        )
+
+        self.assertTrue('j-content s-disabled' not in search._get_raw_page())
+
+    def test_open_browser(self):
+        self.assertIsInstance(TrainSearch._open_browser(), WebDriver)
+
+    def test_parse_schedule(self):
+        search = TrainSearch(
+            departure_station=Station.objects.get(pk=1),
+            arrival_station=Station.objects.get(pk=2),
+            date=datetime.date(2019, 7, 12),
+        )
+
+        raw_page = search._get_raw_page()
+        schedule = search._parse_schedule(raw_schedule=raw_page)
+

a826598e70d5bc7ded6d4851fee582c028e7b54f
Sergey Petrin <spetrin@gmail.com>
2019-08-10 17:43:11 +0300

    Test graphs

--- getseat/views.py
+++ getseat/views.py
@@ -1,3 +1,17 @@
+from django.core import serializers
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from getseat.models import SeatsStatusSnapshot
+
+
+def index(request):
+    return render(request, 'getseat/index.html')
+
+
+def just_json(request):
+    snapshots = SeatsStatusSnapshot.objects.all()
+
+    data = serializers.serialize('json', snapshots)
+
+    return HttpResponse(data, content_type='application/json')
